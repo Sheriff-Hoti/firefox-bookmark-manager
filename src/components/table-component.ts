bf20fde35fcefb8d3980e986fd305efdd2802f2e
Sherif Hoti <sherifhoti1235@hotmail.com>
2024-05-02 03:02:31 +0200

    Added table component and some methods for interacting with browser bookmark

--- src/components/table-component.ts
+++ src/components/table-component.ts
@@ -1,6 +1,10 @@
 import { CSSResultGroup, LitElement, html,unsafeCSS } from 'lit'
-import { customElement } from 'lit/decorators.js'
+import { customElement, property } from 'lit/decorators.js'
 import classes from '../index.css?inline'
+import { Task } from '@lit/task'
+import { Query, search } from '../typed-bookmark-api'
+import { repeat } from 'lit/directives/repeat.js'
+
 
 /**
  * An example element.
@@ -11,44 +15,59 @@ import classes from '../index.css?inline'
 @customElement('table-component')
 export class TableComponent extends LitElement {
 
+  @property()
+  query:Query = {}
+
+  private getBookmarks = new Task(
+    this,
+    //these two later will become @state and have each an input maybe the search mode will be derived from the query
+    //@ts-ignore
+    async ([query, searchmode])=>search(query),
+    ()=>[this.query]
+  )
+
   render() {
-    return html`
-     <div class="overflow-x-auto">
-  <table class="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
-    <thead class="ltr:text-left rtl:text-right">
-      <tr>
-        <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Name</th>
-        <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Date of Birth</th>
-        <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Role</th>
-        <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Salary</th>
-      </tr>
-    </thead>
-
-    <tbody class="divide-y divide-gray-200">
-      <tr>
-        <td class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">John Doe</td>
-        <td class="whitespace-nowrap px-4 py-2 text-gray-700">24/05/1995</td>
-        <td class="whitespace-nowrap px-4 py-2 text-gray-700">Web Developer</td>
-        <td class="whitespace-nowrap px-4 py-2 text-gray-700">$120,000</td>
-      </tr>
-
-      <tr>
-        <td class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Jane Doe</td>
-        <td class="whitespace-nowrap px-4 py-2 text-gray-700">04/11/1980</td>
-        <td class="whitespace-nowrap px-4 py-2 text-gray-700">Web Designer</td>
-        <td class="whitespace-nowrap px-4 py-2 text-gray-700">$100,000</td>
-      </tr>
-
-      <tr>
-        <td class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Gary Barlow</td>
-        <td class="whitespace-nowrap px-4 py-2 text-gray-700">24/05/1995</td>
-        <td class="whitespace-nowrap px-4 py-2 text-gray-700">Singer</td>
-        <td class="whitespace-nowrap px-4 py-2 text-gray-700">$20,000</td>
-      </tr>
-    </tbody>
-  </table>
-</div> 
-    `
+    return this.getBookmarks.render({
+      initial: ()=> html`<div>initial</div>`,
+      pending: ()=> html`<div>pending</div>`,
+      error:(state) => html`${JSON.stringify(state)}`,
+      complete(data) {
+          console.log('the val',data);
+          return html`
+          <div class="h-96 overflow-auto">
+       <table class="min-w-full divide-y-2 divide-gray-200 bg-white text-sm overflow-auto">
+         <thead class="ltr:text-left rtl:text-right">
+           <tr>
+             <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">id</th>
+             <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Date of Birth</th>
+             <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Role</th>
+             <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Salary</th>
+           </tr>
+         </thead>
+     
+         <tbody class="divide-y divide-gray-200">
+            ${
+              repeat(
+                data,
+                value => value.id,
+                value => html`
+                  <tr>
+                    <td class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">${value.id}</td>
+                    <td class="whitespace-nowrap px-4 py-2 text-gray-700">${value.title}</td>
+                    <td class="whitespace-nowrap px-4 py-2 text-gray-700">Web Developer</td>
+                    <td class="whitespace-nowrap px-4 py-2 text-gray-700">$120,000</td>
+                  </tr>
+                `
+              )
+            }
+         </tbody>
+       </table>
+     </div> 
+         `
+      },
+    })
+    
+    
   }
 
   static styles?: CSSResultGroup | undefined = [unsafeCSS(classes)];
--- src/typed-bookmark-api.ts
+++ src/typed-bookmark-api.ts
@@ -1,7 +1,7 @@
 //@ts-ignore
 const bookmarks = browser.bookmarks
 
-type ResultState = "success"|"error";
+// type ResultState = "success"|"error";
 
 type SuccessType<Success> = {
     state: "success",
@@ -16,7 +16,63 @@ type ErrorType<Error> = {
 type Result<Success, Error> = SuccessType<Success> | ErrorType<Error>
 
 //testing purposes it gets the job done
-const smth:Result<string, string> = {
-    state:'success',
-    data:"test"
+// const smth:Result<string, string> = {
+//     state:'success',
+//     data:"test"
+// }
+
+type BookmarkTreeNodeType = "bookmark" | "folder" | "separator"
+
+type BookmarkTreeNodeUnmodifiable = "managed"
+
+type BookmarkTreeNode = {
+    children?: BookmarkTreeNode[],
+    dateAdded?: number,
+    dateGroupModified?: number,
+    id: string,
+    index?: number,
+    parentId?: string,
+    title?: string,
+    type?: BookmarkTreeNodeType,
+    unmodifiable?: BookmarkTreeNodeUnmodifiable,
+    url?: string
+}
+
+type CreateDetails = {
+    index?: number,
+    parentId?: string,
+    title?: string,
+    type?: BookmarkTreeNodeType,
+    url?: string,
+
+}
+
+export type Query = string | {
+    query?: string,
+    url?: string,
+    title?: string
+}
+
+//test with result type
+export async function createBookmark(createDetails: CreateDetails): Promise<Result<BookmarkTreeNode[], string>>{
+    try {
+        const data = await bookmarks.create(createDetails) as BookmarkTreeNode[];
+        return {
+            state:'success',
+            data
+        }
+    } catch (error) {
+        return {
+            state:'error',
+            error: "Error creating the bookmark"
+        }
+    }
+}
+
+export async function create(createDetails: CreateDetails): Promise<BookmarkTreeNode[]> {
+    return bookmarks.create(createDetails) as Promise<BookmarkTreeNode[]>
+}
+
+export async function search(query: Query): Promise<BookmarkTreeNode[]> {
+    return bookmarks.search(query) as Promise<BookmarkTreeNode[]>
 }
